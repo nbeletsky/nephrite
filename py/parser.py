ac129f8431f198fdbe79b41392c0678acdfc81b3
Nick Sinopoli <NSinopoli@gmail.com>
2011-09-17 23:43:13 -0400

    I've no idea.

--- py/parser.py
+++ py/parser.py
@@ -1,19 +1,20 @@
 import os
-import re
+import string
 from pyparsing import *
 
 grammar = '''
 <line> :: <tag> | <comment> | <doctype> | <code> | <include> | <mixin>
 
-<tag>     :: ( <div> | <alpha> ) [ <attribute> ] [ <tag-text> ] | <text-only>
+<tag>     :: <element> [ <attribute> ] [ <tag-text> ] | <text-only>
 <comment> :: <buffered-comment> | <unbuffered-comment> | <block-comment> | <conditional-comment>
 <doctype> :: ( "!!!" | "doctype" ) [ "5" | "html" | "xml" | "default" | "transitional" | "strict" | "frameset" | "1.1" | "basic" | "mobile" ]
 <code>    :: ( <interpolation> | <php> )
 <include> :: "include " <char>
 <mixin>   :: <mixin-define> || <mixin-include>
 
+<element>    :: <div> | <alpha>
 
-<div>   :: "div" [ <div_id> ] [ <div_class>+ ]  | <div_id> | <div_class>+
+<div>       :: "div" [ <div_id> ] [ <div_class>+ ]  | <div_id> | <div_class>+
 <div_id>    :: "#" <alphanum> [ <extra> ]
 <div_class> :: "." <alphanum> [ <extra> ]
 
@@ -24,21 +25,22 @@ grammar = '''
 <digit>      :: "0" | ... | "9"
 <extra>      :: "-" | "_"
 
-<attribute>  :: "(" <key-value> | <text> ")" [ "," ]
-<key-value>  :: <text> "=" ( '"' <text> '"' | "'" <text> "'" )
+<attribute>  :: "(" ( <key-value> | <text> ) [ "," ] ")"
+<key-value>  :: <key> "=" ( '"' <text> '"' | "'" <text> "'" )
 
-<tag-text> :: <text> | ":" ( <div> | <alpha> ) <text> | <indent> <text-block> | <indent> <tag>
+<key>      :: <alpha> [ <interpolation> ]
+<tag-text> :: <text> | ":" <element> <text> | <indent> <text-block> | <indent> <tag>
 <text>     :: <char> [ <interpolation> ]
 
 <text-block> :: "| " <text>
-<char>       :: <any US-ASCII character (octets 0 - 127)>
+<char>       :: <any printable US-ASCII character>
 
 <interpolation>        :: <output-interpolation> | <escape-interpolation>
 <output-interpolation> :: "{{" <variable> "}}"
 <variable>             :: "$" ( <alphanum> | "_" )
 <escape-interpolation> :: "\" <output-interpolation>
 
-<text-only>      :: ( <text-only-tags> [ <attribute> ] | ( <div> | <alpha> ) [ <attribute> ] "." ) <indent> <text>
+<text-only>      :: ( <text-only-tags> [ <attribute> ] | <element> [ <attribute> ] "." ) <indent> <text>
 <text-only-tags> :: "code" | "script" | "textarea" | "style" | "title"
 
 <indent> :: "\n" <tab>
@@ -85,53 +87,70 @@ def detect_token(jade):
     char = Word(printables)
     variable = Literal('$') + Word(alphanums + '_')
 
-    output_interpolation = Literal('{{') + variable + Literal('}}')
-    escape_interpolation = Literal('\\') + output_interpolation
+    output_interpolation = Combine(Literal('{{') + variable + Literal('}}'))
+    escape_interpolation = Combine(Literal('\\') + output_interpolation)
     interpolation = output_interpolation | escape_interpolation
-    text = char + ZeroOrMore(interpolation)
+    text = char + ZeroOrMore(interpolation).setResultsName('interpolation')
 
     text_block = Literal("| ") + text
 
+    tab = Literal('\t') | Literal('  ')
+    indent = Literal('\n') + tab
+
     doctype = oneOf('!!! doctype') + Optional(oneOf('5 html xml default' \
             + 'transitional strict frameset 1.1 basic mobile', True))
-    doctype.setResultsName('doctype')
     doctype.setParseAction(parse_doctype)
 
-    extra = ZeroOrMore('-') + ZeroOrMore('_')
+    div_id = Combine(Literal('#') + Word(alphanums + '_' + '-'))
+    div_class = Combine(Literal('.') + Word(alphanums + '_' + '-'))
 
-    div_id = Literal('#') + Word(alphanums + extra)
-    div_id.setResultsName('div_id')
-    div_class = Literal('.') + Word(alphanums + extra)
-    div_class.setResultsName('div_class')
+    div = (Literal('div') + Optional(div_id).setResultsName('id') \
+        + ZeroOrMore(div_class).setResultsName('class')) \
+        | div_id.setResultsName('id') \
+        | OneOrMore(div_class).setResultsName('class')
 
-    div = (Suppress('div') + Optional(div_id) + ZeroOrMore(div_class)) \
-        | div_id | ZeroOrMore(div_class)
-    div.setResultsName('div')
+    key = Word(alphas) + ZeroOrMore(interpolation).setResultsName('interpolation')
+    key_value = Combine(key + Literal("=") \
+        + ((Literal("'") + text + Literal("'")) | (Literal('"') + text + Literal('"'))))
 
-    key_value = text + Literal("=") \
-        + (Literal("'") + text + Literal("'") | Literal('"') + text + Literal('"'))
-    key_value.setResultsName('key_value')
+    attribute = Suppress('(') \
+        + ZeroOrMore((key_value | text) + Suppress(',')) \
+        + (key_value | text) \
+        + Suppress(')')
 
-    attribute = Suppress('(') + (key_value | text) + Suppress(')') + Optional(',')
+    element = div | Word(alphas)
 
-    # TODO: Fix/finish
-    tag = (div | Word(alpha)) + ZeroOrMore(attribute)
+    tag = Forward()
 
+    tag_text = text | (Literal(':') + element + text) | (indent + text_block) \
+        | (indent + tag)
 
+    text_only_tags = oneOf('code script textarea style title', True)
+    text_only_tags.setResultsName('text_only_tags')
 
+    text_only = ((text_only_tags + ZeroOrMore(attribute)) \
+        | (element + ZeroOrMore(attribute) + Literal('.'))) \
+        + indent + text
+
+    tag << ((element.setResultsName('element') \
+        + ZeroOrMore(attribute).setResultsName('attribute') \
+        + ZeroOrMore(tag_text).setResultsName('tag_text')) \
+        | text_only.setResultsName('text_only'))
+    tag.setParseAction(parse_tag)
 
     include = Suppress(Literal('include')) + char
-    include.setResultsName('include')
     include.setParseAction(parse_include)
 
-    line = doctype | include
-    source = OneOrMore(line)
+    line = doctype.setResultsName('doctype') | include.setResultsName('include') \
+        | tag.setResultsName('tag')
+    source = OneOrMore(line).setResultsName('line')
     parsed = source.parseString(jade)
+    print(parsed.dump() + '\n')
 
     return ' '.join(parsed)
 
-def parse_doctype(results):
-    results = " ".join(results)
+def parse_doctype(orig, loc, toks):
+    results = ' '.join(toks)
 
     doctypes = {
         '5': '<!DOCTYPE html>',
@@ -153,8 +172,12 @@ def parse_doctype(results):
     doctype = right.strip().lower()
     return doctypes.get(doctype, doctypes['default'])
 
-def parse_include(results):
-    include = ''.join(results) + '.jade'
+def parse_include(orig, loc, toks):
+    include = ''.join(toks) + '.jade'
     if not os.path.isfile(include):
         raise ParseFatalException("File `%s` not found." % include)
     return parse(include)
+
+def parse_tag(orig, loc, toks):
+    #print(results)
+    return toks;
